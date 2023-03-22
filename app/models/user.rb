96b39268685f2c2effbea8bfea9a4df23db6612f
Hunter Wood <hunter674530@gmail.com>
2023-03-22 00:08:30 -0600

    working seeds/realtionships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :notes, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :chats, through: :messages
+
+  has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,29 +3,20 @@
 User.destroy_all
 Chat.destroy_all
 
-User.create!()
-User.create!()
-User.create!()
+user1 = User.create!(username: 'user1', password: 'password1')
+user2 = User.create!(username: 'user2', password: 'password2')
+user3 = User.create!(username: 'user3', password: 'password3')
 
-Chat.create!()
-Chat.create!()
-Chat.create!()
-Chat.create!()
+Chat.create!(name: 'birthday' )
 
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
-Message.create!()
 
-Note.create!()
-Note.create!()
-Note.create!()
-Note.create!()
-Note.create!()
-Note.create!()
+Message.create!(content: 'did you hear?', user_id: user1.id, chat_id: 1)
+Message.create!(content: 'yes', user_id: user2.id, chat_id: 1)
+Message.create!(content: 'will be there', user_id: user3.id, chat_id: 1)
+
+
+Note.create!(content: 'this a note', user_id: 1)
+Note.create!(content: 'this might be a note', user_id: 1)
+Note.create!(content: 'definitely a note', user_id: 3)
+Note.create!(content: 'this might be aaa note', user_id: 2)
+Note.create!(content: 'note1', user_id: 1)
