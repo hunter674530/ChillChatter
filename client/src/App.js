2ebf86151c2ec9dc1c4e52f6d893851b980e08d6
Hunter Wood <hunter674530@gmail.com>
2023-03-27 13:56:51 -0600

    working router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import { useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Chats from "./Chats";
+import Notes from "./Notes";
+import NoPage from "./NoPage";
 
 function App() {
   const [user, setUser] = useState({});
@@ -12,9 +19,17 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Login changeUser={changeUser} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login changeUser={changeUser} />} />
+          <Route path="chats" element={<Chats />} />
+          <Route path="notes" element={<Notes />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
---
+++ client/src/NavBar.js
@@ -0,0 +1,28 @@
+import { Outlet, Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/chats">Chats</Link>
+          </li>
+          <li>
+            <Link to="/notes">Notes</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default NavBar;
